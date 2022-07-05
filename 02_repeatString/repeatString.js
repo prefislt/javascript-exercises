676f5796b262ecc6390ff11216a2889cd685c28e
Emilis Veliullajev <prefis@pm.me>
2022-07-05 10:20:59 +0300

    Complete exercises 01, 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    if (num < 0) { return "ERROR"; } else
+    if (num == 0) { return ""; }
 
+    let i = 0;
+    let stringArray = [];
+    while (num > i) {
+        stringArray[i] = string;
+        i++;
+    }
+    return stringArray.join("");
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+function reverseString(string) {
+    let stringLength = string.length;
+    let lettersArray = [];
+    let i = 0;
 
-};
+    while (i <= stringLength) {
+        lettersArray[i] = string.charAt(stringLength - (i+1));
+        i++;
+    }
+
+    return lettersArray.join("");
+
+}
 
 // Do not edit below this line
 module.exports = reverseString;
